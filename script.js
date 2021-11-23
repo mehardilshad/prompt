83ebb0a76c4ec6ba9509b09ce3e2067d9b4568d0
mehardilshad <mehardilshad@gmail.com>
2021-11-23 17:54:30 +0530

    okay

---
+++ script.js
@@ -0,0 +1,8 @@
+function sum() {
+    var num1 = prompt("enter the first number")
+    var num2 = prompt("enter the second number")
+    let c = parseInt (num1)+ parseInt (num2);
+    console.log(c);
+    return c;
+}
+alert("sum is "+sum());         
